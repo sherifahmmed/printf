97490d041008e3ce52f8d816b9a8342c741418af
shaaareeef <sherifkahraba500@gmail.com@gmail.com>
2023-08-22 16:29:08 -0700

    a

--- print_number.c
+++ print_number.c
@@ -1,3 +1,12 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: the integer to prints.
+ * Return: empty
+ */
+
 void print_number(int n)
 {
 	unsigned int n1;
@@ -5,10 +14,11 @@ void print_number(int n)
 	n1 = n;
 
 	if (n < 0)
-	{	
+	{
 		putchar('-');
 		n1 = -n;
 	}
+
 	if (n1 / 10 != 0)
 	{
 		print_number(n1 / 10);
