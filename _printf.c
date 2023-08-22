14dd8105d35ce51c78d002b8c1a6b9c9d0c7e5e9
Taha-00 <traitaha@gmail.com>
2023-08-22 13:21:02 -0700

    nc

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+/* more headers goes there */
+
+/**
+ *_printf - a function
+ *@format: char string
+ *@...: arguments
+ * Return: int
+ */
+
+int _printf(const char *format, ...)
+
--- main.h
+++ main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
-
+int _printf(const char *format, ...);
 
 
 
