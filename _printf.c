0472b0f36daa5dd710e3ef8ef3e4eab0cdeb9095
Taha-00 <traitaha@gmail.com>
2023-08-22 14:57:14 -0700

    nc

--- _printf.c
+++ _printf.c
@@ -16,6 +16,10 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%' && format[i + 1] == 'c')
